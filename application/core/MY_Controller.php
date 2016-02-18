cd311d56fa30f452fd90c9e4bec6eb96436af020
Carson Roscoe <CarsonRoscoe7@gmail.com>
2016-02-17 20:38:04 -1000

    Starting website look & seperating into template

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * core/MY_Controller.php
+ *
+ * Default application controller
+ *
+ * @author		JLP
+ * @copyright           2010-2015, James L. Parry
+ * ------------------------------------------------------------------------
+ */
+class Application extends CI_Controller {
+
+    protected $data = array();      // parameters for view components
+    protected $id;                  // identifier for our content
+    protected $choices = array(
+        'Homepage' => '/', 'Profile' => '/profile', 'Stock' => '/stock'
+    );
+
+    /**
+     * Constructor.
+     * Establish view parameters & load common helpers
+     */
+
+    function __construct() {
+        parent::__construct();
+        $this->data = array();
+        $this->data['pagetitle'] = "StockWatch";
+        $this->data['pageheader'] = "StockWatch";
+    }
+
+    /**
+     * Render this page
+     * Used on all. We need to load data into content in the controller
+     */
+    function render() {
+        //$this->data['navbar'] = build_menu_bar($this->choices);
+        $this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
+        $this->data['navigation'] = $this->parser->parse($this->data['pagenavigation'], $this->data, true);
+        $this->data['data'] = &$this->data;
+        $this->parser->parse('template', $this->data);
+    }
+}
