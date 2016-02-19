b5a624e292775294ceefe632a8a413fb6592ba09
JaegarSarauer <jaegars@hotmail.com>
2016-02-18 17:21:46 -0800

    Pushing working query, needs to implement with new DB

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -8,9 +8,21 @@ class Welcome extends Application {
 	 */
 	public function index()
 	{
+            //var_dump($this->players->getDatabaseData());
             //$this->load->view('welcome_message');
-            $this->data['pagebody'] = 'index';
+            $this->data['playersQuery'] = $this->queryPlayers();
+            //$this->load->('playersQuery', array('data' => $this->players->getDatabaseData()));
+            $this->data['pagebody'] = 'index';//new DBQuery().getDatabaseData();//'index';
             $this->data['pagenavigation'] = 'navigation';
             $this->render();
 	}
+        
+        public function queryPlayers() {
+            $data = $this->players->getDatabaseData();
+            $res = '';
+            foreach($data as $player) {
+                $res .= '<tr>  <td>' . $player["Player"] . '</td><td>' . $player["Cash"] . '</td></tr>';
+            }
+            return $res;
+        }
 }
