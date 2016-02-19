52de8d829c9144d7fd24bf726c8ae68ac7fbf0a3
Allen Tsang <allen_tsang@hotmail.com>
2016-02-19 03:22:15 -0800

    Minor bugfixes

--- application/views/_template.php
+++ application/views/_template.php
@@ -4,7 +4,7 @@
 		<title id="pageTitle">{pagetitle}</title>
 		<meta charset="UTF-8" />
 		<link type="text/css" rel="stylesheet" href="/assets/css/style.css" />
-                <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
 		<div id="header">
