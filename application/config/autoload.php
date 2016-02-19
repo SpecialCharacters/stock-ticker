1766e9c89b113540fc0e0bb2dd81a195861c16f6
Dhivya Manohar <dhivya_shantini@hotmail.com>
2016-02-19 15:40:38 -0800

    minor change is in autoload.php

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -85,7 +85,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array('url');
 
 /*
 | -------------------------------------------------------------------
